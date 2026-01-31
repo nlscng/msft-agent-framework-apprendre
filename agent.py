a2dec8d7bf08a3fa9e23296497f2a5815abf97c0
ncheng <nlscng.g@gmail.com>
2026-01-30 19:44:34 -0600

    Ruuning agent with streaming snippet.

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 import asyncio
+from py_compile import main
 from agent_framework.azure import AzureOpenAIChatClient
 from azure.identity import AzureCliCredential
 
@@ -7,8 +8,20 @@ agent = AzureOpenAIChatClient(credential=AzureCliCredential()).create_agent(
     name="Joker"
 )
 
-async def main():
+
+# running the agent
+async def run_agent():
     result = await agent.run("Tell me a joke about a pirate.")
     print(result.text)
 
-asyncio.run(main())
+asyncio.run(run_agent())
+
+
+# running the agent with streaming
+async def run_agent_streaming():
+    async for update in agent.run_stream("Tell me a joke about a pirate."):
+        if update.text:
+            print(update.text, end="", flush=True)
+    print()  # New line after streaming is complete
+
+asyncio.run(run_agent_streaming())
